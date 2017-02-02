e50fccaaccee0f40ace6bb49c8ba65a18f670f5a
byYottaFLOPS <jonas.peeters@icloud.com>
2017-02-02 14:04:55 +0100

    Added everything

--- send.php
+++ send.php
@@ -1,25 +1,26 @@
 <html>
 <head>
     <?php
-        ini_set( 'display_errors', 1 );
-        
-        error_reporting( E_ALL );
-        
-        $from = $_GET["from"];
-        
-        $to = $_GET["to"];
-        
-        $subject = $_GET["subject"];
-        
-        $message = $_GET["message"];
-        
-        $headers = "From:" . $from;
-        
-        if (subject == "modinstaller") {
-            mail($to,$subject,$message, $headers);
-        }
-        
-        echo "<meta http-equiv='refresh' content='5; URL=index.html'>";
+        error_reporting(-1);
+        ini_set('display_errors', 'On');
+        set_error_handler("var_dump");
+
+        $from = 'info@mine-modinstaller.esy.es';
+
+        $to = 'jonaspeeters.de@gmail.com';
+
+        $subject = $_GET['subject'];
+
+        $message = str_replace('|', "\r\n", $_GET['message']);
+
+        $headers = 'From:' . $from;
+
+        if(mail($to, $subject, $message, $headers))
+            echo('t');
+        else
+            echo('f');
+
+        echo "<meta http-equiv='refresh' content='0; URL=index.html?success=true'>";
     ?>
 </head>
 </html>
